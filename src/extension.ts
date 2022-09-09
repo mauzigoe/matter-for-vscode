38309c53e3c45e90b99e746ac956f3fc84c6adf6
Leon Frangen <lefr@uni-bremen.de>
2022-09-10 01:16:04 +0200

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -15,8 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 
-	let matterInstance: matter.Matter = new matter.Matter({});
-	
+	let matterInstance: matter.Matter = new matter.Matter();
 	context.subscriptions.push(vscode.commands.registerCommand('matter-for-vscode.runMatlabFile', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
--- src/matter/matter.ts
+++ src/matter/matter.ts
@@ -1,27 +1,32 @@
+import { urlToOptions } from '@vscode/test-electron/out/util';
+import { log } from 'console';
 import * as vscode from 'vscode';
 
-interface MatlabTerminal {
-    terminal?: vscode.Terminal
-}
-
-export class Matter implements MatlabTerminal{
-    static terminalSettings: vscode.TerminalOptions = { 
-        name : "Matlab Terminal",
-        shellPath: "/usr/bin/env",
-        shellArgs: ["matlab", "-nodesktop", "-nosplash"]
-    }
+export class Matter {
     public terminal: vscode.Terminal;
-    constructor(matter: MatlabTerminal){
-        if (matter.terminal) {
-            this.terminal = matter.terminal;
+
+    constructor(){
+
+        // configuraiton wir irgendwie nicht initialisiert
+        let options : Function[] = [this.getNoDesktopOption,this.getNoSplashOption, this.getLicenseOption, this.getLogfileOption];
+        let matlabArgs: any[] = this.getOptionsForMatlabTerminal(options);
+        let terminalSettings =  { 
+            name : "Matlab Terminal",
+            shellPath: "/usr/bin/env",
+            shellArgs: ["matlab",  ...matlabArgs]
         }
-        else {
-            this.terminal = vscode.window.createTerminal(Matter.terminalSettings); 
-        }
+        this.terminal = vscode.window.createTerminal(terminalSettings); 
     };
 
-    public createMatlabTerminal(): void {
-        this.terminal = vscode.window.createTerminal(Matter.terminalSettings);
+    private createMatlabTerminal(): void {
+        let options : Function[] = [this.getNoDesktopOption,this.getNoSplashOption, this.getLicenseOption, this.getLogfileOption];
+        let matlabArgs: string[] = this.getOptionsForMatlabTerminal(options)
+        let terminalSettings =  { 
+            name : "Matlab Terminal",
+            shellPath: "/usr/bin/env",
+            shellArgs: ["matlab",  ...matlabArgs]
+        }
+        this.terminal = vscode.window.createTerminal(terminalSettings);
     }
 
     public getTerminalIfNotExisting(): void {
@@ -38,8 +43,43 @@ export class Matter implements MatlabTerminal{
 
     public runFile(){
         let matlabFile: string = vscode.window.activeTextEditor?.document.getText() ?? "";
+        this.createTerminalIfNotExisting();
         this.terminal.sendText(matlabFile);
         this.terminal.show();
 
     };
-};
+
+    public getOptionsForMatlabTerminal(options : Function[]){
+        return options.map(this.getOptions).filter(option => option!=undefined)
+    }
+
+    public getOptions(optionFunction: Function) {
+        return optionFunction()
+    }
+
+    public getNoDesktopOption() {
+        return "-nodesktop";
+    }
+    
+    public getNoSplashOption() {
+        return "-nosplash";
+    }
+
+    public getLicenseOption() {
+        let isUsed: any = vscode.workspace.getConfiguration().get('getLicenseOption.isUsed');
+        let path: any = vscode.workspace.getConfiguration().get('getLicenseOption.path');
+        if (isUsed) {
+            return `-c ${path}`
+        }
+        return undefined
+    }
+
+    public getLogfileOption() {
+        let isUsed: any = vscode.workspace.getConfiguration().get('getLogfileOption.isUsed');
+        let path: any = vscode.workspace.getConfiguration().get('getLogfileOption.path');
+        if (isUsed) {
+            return `-logfile ${path}`
+        }
+        return undefined
+    }
+}
